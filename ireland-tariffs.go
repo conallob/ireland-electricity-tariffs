cabf877cbbc589e858362abef53c73773b945634
Conall O'Brien <conall@conall.net>
2023-09-14 11:40:50 +0100

    Add MicroGeneration attribute to TariffPrice
    Add MicroGeneration to TariffPrice, populate across both tariff entries
    
    Create additional tariff entry from 2023-09-15, which increased
    microgeneration rate

--- ireland-tariffs.go
+++ ireland-tariffs.go
@@ -7,11 +7,12 @@ import (
 )
 
 type TariffPrice struct {
-	Day      float64 `json:"day"`      // Price per kWh, in €
-	Peak     float64 `json:"peak"`     // Price per kWh, in €
-	Night    float64 `json:"night"`    // Price per kWh, in €
-	VATRate  float64 `json:"VAT"`      // Applicable VAT Rate, in decimal form
-	Discount float64 `json:"discount"` // Applied Discount, in decimal form
+	Day      	float64 `json:"day"`      		// Price per kWh, in €
+	Peak     	float64 `json:"peak"`     		// Price per kWh, in €
+	Night    	float64 `json:"night"`    		// Price per kWh, in €
+	MicroGeneration	float64 `json:"microgeneration"`    	// Price per kWh, in €
+	VATRate  	float64 `json:"VAT"`      		// Applicable VAT Rate, in decimal form
+	Discount 	float64 `json:"discount"` 		// Applied Discount, in decimal form
 }
 
 type ElectricityTariff struct {
@@ -30,11 +31,25 @@ func main() {
 			PlanName: "Smart Data - 15",
 			PlanShortName: "energia-smart-15",
 			Price: TariffPrice{
-				Day:      0.4576,
-				Peak:     0.4794,
-				Night:    0.2450,
-				VATRate:  0.09,
-				Discount: 0.15,
+				Day:		0.4576,
+				Peak:     	0.4794,
+				Night:		0.2450,
+				MicroGeneration: 0.18,
+				VATRate:  	0.09,
+				Discount: 	0.15,
+			},
+		},
+		time.Date(2023, time.September, 15, 0, 0, 0, 0, time.UTC): {
+			Provider: "Energia",
+			PlanName: "Smart Data - 15",
+			PlanShortName: "energia-smart-15",
+			Price: TariffPrice{
+				Day:		0.4576,
+				Peak:     	0.4794,
+				Night:		0.2450,
+				MicroGeneration: 0.24,
+				VATRate:  	0.09,
+				Discount: 	0.15,
 			},
 		},
 			time.Date(2023, time.October, 2, 0, 0, 0, 0, time.UTC): {
@@ -42,11 +57,12 @@ func main() {
 			PlanName: "Smart Data - 15",
 			PlanShortName: "energia-smart-15",
 			Price: TariffPrice{
-				Day:      0.3731,
-				Peak:     0.213,
-				Night:    0.245,
-				VATRate:  0.09,
-				Discount: 0.15,
+				Day:	  		0.3731,
+				Peak:	  		0.213,
+				Night:	  		0.245,
+				MicroGeneration:	0.24,
+				VATRate:  		0.09,
+				Discount: 		0.15,
 			},
 		},
 
